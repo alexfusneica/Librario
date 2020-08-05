34709e5cc150ccacf4a6c7fcff7b115fde2cde22
alexFusneica <alexandruionut.fusneica@amaris.com>
2020-08-05 15:36:50 +0200

    added Search controller

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -12,5 +12,9 @@ namespace Project1.Controllers
         {
             return Content($"Sono Detail, ho ricevuto l'id {id}");
         }
+        public IActionResult Search(string title)
+        {
+            return Content($"Hai creato {title}");
+        }
     }
 }
